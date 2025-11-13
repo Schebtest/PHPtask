dc37e4e7ff48cf1b285ea30580e1af5ef11ec20d
John Doe <johndoe@example.com>
2025-11-13 21:52:26 +0600

    13

---
+++ 942/Аутентификация и авторизация с COOKIE_6/Задание_1-3/Раздаточный/person/personnel.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Программирование на языке PHP</title>
+</head>
+<body>
+
+	<h1>Аутентификация и авторизация с COOKIE</h1>
+
+	<!-- меню сайта -->
+	<p>
+		<!-- ... -->
+	</p>		
+	
+	<h2>Информация о преподавателе</h2>
+
+	<?php
+
+		// ...
+
+	?>
+	
+
+</body>
+</html>
---
+++ 942/Аутентификация и авторизация с COOKIE_6/Задание_1-3/Раздаточный/reg.php
@@ -0,0 +1,5 @@
+<?php
+	// подключение к базе данных
+	$mysqli = new mysqli ('localhost', 'root', '', 'db_distance_user');
+
+	// ...  
