20b7c986b962e1621476d1e6e74d6ad64bc46613
Scheb9 <johndoe@example.com>
2025-11-26 09:33:27 +0600

    15

---
+++ 942/COOKIE и SESSION в PHP. Важное_8/Задание_2/Раздаточный/form.php
@@ -0,0 +1,34 @@
+<?php
+
+	// ...
+
+?>
+
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Программирование на языке PHP</title>
+</head>
+<body>
+
+	<h1>Безопасность приложений</h1>
+	
+	<?php
+		// вывод формы с данными массива сессии
+		echo <<<HERE
+			<form action="" method="post">
+				Название: <input type="text" name="name" value=""><p>
+				Алиас: <input type="text" name="alias" value=""><p>
+				Страна: <input type="text" name="country" value=""><p>
+				
+				<textarea name="content"></textarea><p>
+						
+				<input type="submit">
+			</form>
+		HERE;
+	?>
+
+
+</body>
+</html>
