baec1e6b289c3ecfb88b9ee0a179118669e977b5
John Doe <johndoe@example.com>
2025-12-26 10:43:11 +0600

    25

---
+++ 944/13_Создание экземпляра класса (объекта)/Задание_2/Раздаточный/index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Программирование на языке PHP</title>
+</head>
+<body>
+	<h1>Объектно-ориентированное программирование</h1>
+	
+	<?php
+		// подключаем массив team
+		include "team.php";
+
+		// итерируем массив
+		foreach ($team as $item) {
+
+			// ...			
+
+		}
+
+		// просмотр результата
+		echo "<pre>";
+
+		// ...
+
+		echo "</pre>";
+	?>
+
+
+</body>
+</html>
---
+++ 944/13_Создание экземпляра класса (объекта)/Задание_2/Раздаточный/team.php
@@ -0,0 +1,13 @@
+<?php
+# phpMyAdmin SQL Dump
+# version 4.0.10
+#
+# Структура таблицы `team`
+$team = array(
+  array('id_team' => '1','name' => 'Aerosmith','alias' => 'Aerosmith','country' => 'США','content' => '','date' => '1970','style' => 'хард-рок','path' => 'assets/teams/aerosmith.jpg','note' => NULL),
+  array('id_team' => '2','name' => 'Pink Floyd','alias' => 'PinkFloyd','country' => 'Великобритания','content' => '','date' => '1965','style' => 'психоделический-рок','path' => 'assets/teams/pink-floyd.jpg','note' => NULL),
+  array('id_team' => '3','name' => 'The Beatles','alias' => 'TheBeatles','country' => 'Великобритания','content' => '','date' => '1960','style' => 'рок-н-ролл','path' => 'assets/teams/beatles.jpg','note' => NULL),
+  array('id_team' => '4','name' => 'AC/DC','alias' => 'ACDC','country' => 'Австралия','content' => '','date' => '1973','style' => 'хард-блюз-рок','path' => 'assets/teams/acdc.jpg','note' => NULL),
+  array('id_team' => '5','name' => 'Scorpions','alias' => 'Scorpions','country' => 'ФРГ','content' => '','date' => '1965','style' => 'хард-рок','path' => 'assets/teams/scorpions_.jpg','note' => NULL),
+  array('id_team' => '6','name' => 'Ленинград','alias' => 'Leningrad','country' => 'Россия','content' => '','date' => '1997','style' => 'ска, фолк, панк','path' => 'assets/teams/leningrad.jpg','note' => NULL)
+);
